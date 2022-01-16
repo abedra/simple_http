5c64de3903fe3f9876003441032cebc09e7008c8
Aaron Bedra <aaron@aaronbedra.com>
2022-01-15 21:49:29 -0600

    Update README and examples

--- example/jsonplaceholder/main.cpp
+++ example/jsonplaceholder/main.cpp
@@ -5,12 +5,13 @@
 int main() {
   SimpleHttp::Client client;
   SimpleHttp::HttpUrl url{"https://jsonplaceholder.typicode.com/users"};
-  const std::optional<SimpleHttp::HttpResponse> &maybeResponse = client.get(url);
-
-  if (maybeResponse) {
-    auto parsed = nlohmann::json::parse(maybeResponse.value().body.value());
-    std::cout << parsed[0]["id"] << std::endl;
-  } else {
-    std::cout << "Failed GET for: " << url.value() << std::endl;
-  }
+  client.get(url).template match<void>(
+      [&url](const SimpleHttp::HttpFailure &failure){
+        std::cout << "Failed GET for: " << url.value() << std::endl;
+      },
+      [](const SimpleHttp::HttpSuccess &success){
+        auto parsed = nlohmann::json::parse(success.body().value());
+        std::cout << parsed[0]["id"] << std::endl;
+      }
+  );
 }
