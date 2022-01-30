2c559e9925e8a514863387ac524dd8b13e5598c8
Aaron Bedra <aaron@aaronbedra.com>
2022-01-29 18:06:14 -0600

    More complete example

--- example/jsonplaceholder/main.cpp
+++ example/jsonplaceholder/main.cpp
@@ -1,24 +1,40 @@
 #include <iostream>
-#include "nlohmann/json.hpp"
-#include "simple_http.hpp"
+#include <nlohmann/json.hpp>
+#include <simple_http.hpp>
+
+struct Failure final {
+  std::string url;
+  std::string value;
+};
+
+struct Id final {
+  int value;
+};
 
 int main() {
   SimpleHttp::Client client;
   SimpleHttp::HttpUrl url{"https://jsonplaceholder.typicode.com/users"};
-  client.get(url).template match<void>(
+  auto result = client.get(url).template match<std::variant<Failure, Id>>(
       [&url](const SimpleHttp::HttpFailure &failure){
-        failure.template match<void>(
+        return failure.template match<std::variant<Failure, Id>>(
             [&url](const SimpleHttp::HttpConnectionFailure &connectionFailure) {
-              std::cout << "Failed GET for: " << url.value() << ", " << connectionFailure.value() << std::endl;
+              return Failure{url.value(),connectionFailure.value()};
             },
             [&url](const SimpleHttp::HttpResponse &semanticFailure) {
-              std::cout << "Failed GET for: " << url.value() << ", " << semanticFailure << std::endl;
+              return Failure{url.value(), semanticFailure.body.value()};
             }
         );
       },
       [](const SimpleHttp::HttpSuccess &success){
         auto parsed = nlohmann::json::parse(success.body().value());
-        std::cout << parsed[0]["id"] << std::endl;
+        return Id{parsed[0]["id"]};
       }
   );
+
+  if (std::holds_alternative<Failure>(result)) {
+    Failure failure = std::get<Failure>(result);
+    std::cout << "Failed HTTP request for <" << failure.url << ">: " << failure.value << std::endl;
+  } else {
+    std::cout << "Result: " << std::get<Id>(result).value << std::endl;
+  }
 }
